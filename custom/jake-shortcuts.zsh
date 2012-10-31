b401c7ed39f6252349904fb9ef115a51c7aa696f
Jake Mauer <jake@jakemauer.com>
2012-10-31 15:38:48 -0500

    Added path for jbilling ui

--- custom/jake-shortcuts.zsh
+++ custom/jake-shortcuts.zsh
@@ -1,8 +1,9 @@
 # Project Shortcuts
 # -----------------
-alias jb="~/Dropbox/AppDirect/Sites/jBilling"
-alias jbui="~/Dropbox/AppDirect/Sites/jBilling UI"
-alias ad="~/Dropbox/AppDirect/Sites/AppDirect"
+
+alias jb="cd ~/Dropbox/AppDirect/Sites/jBilling"
+alias jbui="cd ~/Dropbox/AppDirect/Sites/jBilling\ UI"
+alias ad="cd ~/Dropbox/AppDirect/Sites/AppDirect"
 
 # SSH & Tunnels
 # -------
