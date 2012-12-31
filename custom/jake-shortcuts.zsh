26f6883c044c2c1689f6a099e87e4e7bf2247bdf
Jake Mauer <jake@jakemauer.com>
2012-12-31 01:16:50 -0600

    Changed channel docs location

--- custom/jake-shortcuts.zsh
+++ custom/jake-shortcuts.zsh
@@ -5,7 +5,7 @@ alias jb="cd ~/Dropbox/AppDirect/Sites/jBilling"
 alias jbui="cd ~/Dropbox/AppDirect/Sites/jBilling\ UI"
 alias ad="cd ~/Dropbox/AppDirect/Sites/AppDirect"
 alias adi="cd ~/Dropbox/AppDirect/Sites/AppDirect/site/adi"
-alias adcd="cd ~/Dropbox/AppDirect/Sites/channel-docs"
+alias adcd="cd ~/Dropbox/AppDirect/Sites/Channel\ Docs"
 
 # Rails Projects
 # --------------
